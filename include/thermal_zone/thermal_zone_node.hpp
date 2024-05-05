48e101eec14324bbf899e22929337c6894ac5fc7
Nathanael Gandhi <nat11public@gmail.com>
2024-05-05 15:23:59 +0000

    feat(temperature): populate message for each found zone

--- include/thermal_zone/thermal_zone_node.hpp
+++ include/thermal_zone/thermal_zone_node.hpp
@@ -3,6 +3,8 @@
 #include <rclcpp/rclcpp.hpp>
 #include <string>
 
+#include "thermal_zone_interfaces/msg/thermal_zone.hpp"
+
 class ThermalZonePublisherNode : public rclcpp::Node
 {
 public:
@@ -11,4 +13,8 @@ public:
 
 protected:
 private:
+  std::vector<thermal_zone_interfaces::msg::ThermalZone> GetZoneMsgVector(void);
+  thermal_zone_interfaces::msg::ThermalZone GetZoneMsg(std::string key, uint8_t zone_index);
+  double GetZoneTemperature(std::string prefix);
+  uint8_t CountMatchingDirectories(const std::string & pattern);
 };
