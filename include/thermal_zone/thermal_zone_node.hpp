09a1d57fa36b8b2254ef044b3511c6b5440e9c50
Nathanael Gandhi <nat11public@gmail.com>
2024-05-16 10:13:40 +1000

    feat: publish all ThermalZone & node hk messages

--- include/thermal_zone/thermal_zone_node.hpp
+++ include/thermal_zone/thermal_zone_node.hpp
@@ -4,6 +4,7 @@
 #include <string>
 
 #include "thermal_zone_interfaces/msg/thermal_zone.hpp"
+#include "thermal_zone_interfaces/msg/thermal_zone_node_hk.hpp"
 
 class ThermalZoneNode : public rclcpp::Node
 {
@@ -13,6 +14,14 @@ public:
 
 protected:
 private:
+  size_t thermal_pub_count_;
+  rclcpp::TimerBase::SharedPtr timer_1s_;
+  rclcpp::TimerBase::SharedPtr timer_10s_;
+  rclcpp::Publisher<thermal_zone_interfaces::msg::ThermalZone>::SharedPtr publisher_thermal_;
+  rclcpp::Publisher<thermal_zone_interfaces::msg::ThermalZoneNodeHk>::SharedPtr publisher_node_hk_;
+
+  void timer_1s_callback(void);
+  void timer_10s_callback(void);
   std::vector<thermal_zone_interfaces::msg::ThermalZone> GetZoneMsgVector(void);
   thermal_zone_interfaces::msg::ThermalZone GetZoneMsg(std::string key, uint8_t zone_index);
   double GetZoneTemperature(std::string prefix);
